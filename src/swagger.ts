b1acf45a17cf01a4c0a232685ad3d86573a8abdf
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-01-29 20:21:58 +0530

    render url

--- src/swagger.ts
+++ src/swagger.ts
@@ -12,7 +12,7 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: 'http://localhost:3000',
+                url: 'https://gadget-inventory-4qyd.onrender.com',
                 description: 'Development server',
             },
         ],
